cc2859026fd422735170b612c608c3a85bb99492
golic98 <00214917@uca.edu.sv>
2025-06-22 14:58:13 -0600

    update

--- service/src/routes/route.main.js
+++ service/src/routes/route.main.js
@@ -19,7 +19,7 @@ router.get("/profile/:id", authRequired, getOneProfile);
 router.put("/profile/:id", authRequired, updateProfile);
 router.post("/payVigilance", authRequired, addPayVigilance);
 router.get("/allPay", authRequired, getAllPay);
-router.post("/createUser", authRequired, createUserByAdmin);
+router.post("/createUser", validateSchema(registerSchema), createUserByAdmin);
 
 
 export default router;
