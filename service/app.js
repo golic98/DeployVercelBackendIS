591edd06eca4d250229175014d1d892aedeccecf
golic98 <00214917@uca.edu.sv>
2025-06-30 12:09:22 -0600

    update

--- service/app.js
+++ service/app.js
@@ -5,9 +5,6 @@ import { connectiondb } from "./src/config/dbConnection.js";
 import cookieParser from "cookie-parser";
 import taskRoute from "./src/routes/route.task.js";
 import cors from "cors";
-import swaggerUi from "swagger-ui-express";
-import swaggerJsdoc from "swagger-jsdoc";
-import swaggerOptions from "./src/swagger/swagger.options.js";
 import vigilantRoute from "./src/routes/route.vigilant.js";
 
 const app = express();
@@ -35,16 +32,7 @@ app.use("/meg", mainRouter);
 app.use("/meg", taskRoute);
 app.use("/meg", vigilantRoute);
 
-const swaggerSpec = swaggerJsdoc(swaggerOptions);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
 const PORT = process.env.PORT || 1200;
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 1200;
-  app.listen(PORT, () => {
-      console.log("El servidor está trabajando en el puerto: " + PORT);
-      console.log("Documentación Swagger disponible en: http://localhost:" + PORT + "/api-docs");
-  });
-}
-
-export default app;
+app.listen(PORT, () => {
+    console.log("El servidor está trabajando en el puerto: " + PORT);
+});
