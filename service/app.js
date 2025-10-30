9687805f462614422b5b4764697870503ff58921
golic98 <00214917@uca.edu.sv>
2025-10-30 10:24:28 -0600

    actualizando app.js

--- service/app.js
+++ service/app.js
@@ -13,7 +13,7 @@ connectiondb();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: 'https://deploy-vercel-frontendt-is.vercel.app/', credentials: true }))
+app.use(cors({ origin: 'https://deploy-vercel-frontendt-is.vercel.app', credentials: true }))
 app.use("/api", mainRouter);
 app.use("/api", taskRoute);
 app.use("/api", vigilantRoute);
