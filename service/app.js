47ab09838fd60c3e0f35e511b5f47bc3bf15d5c2
golic98 <00214917@uca.edu.sv>
2025-10-30 09:51:46 -0600

    habilitando config de cors de app.js

--- service/app.js
+++ service/app.js
@@ -5,6 +5,7 @@ import { connectiondb } from "./src/config/dbConnection.js";
 import cookieParser from "cookie-parser";
 import taskRoute from "./src/routes/route.task.js";
 import vigilantRoute from "./src/routes/route.vigilant.js";
+import cors from "cors";
 
 const app = express();
 
@@ -12,6 +13,7 @@ connectiondb();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({ origin: 'https://deploy-vercel-frontendt-is.vercel.app/', credentials: true }))
 app.use("/api", mainRouter);
 app.use("/api", taskRoute);
 app.use("/api", vigilantRoute);
