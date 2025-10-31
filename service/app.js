5325b9855fe57292759b6e6775fb7887ba961826
golic98 <00214917@uca.edu.sv>
2025-10-31 07:59:51 -0600

    actualizando mensaje de bienvenida

--- service/app.js
+++ service/app.js
@@ -14,7 +14,7 @@ connectiondb();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: 'https://deploy-vercel-frontendt-is.vercel.app', credentials: true }));
-app.get("/", (req, res) => res.status(200).send("Bienvenido"));
+app.get("/", (req, res) => res.status(200).send("Bienvenido a nuestro servidor."));
 app.use("/api", mainRouter);
 app.use("/api", taskRoute);
 app.use("/api", vigilantRoute);
