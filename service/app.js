17ee835b42cb0d9acceb51c867a074ce7b8a045d
golic98 <00214917@uca.edu.sv>
2025-10-31 08:08:12 -0600

    actualizando archivo vercel

--- service/app.js
+++ service/app.js
@@ -5,7 +5,6 @@ import { connectiondb } from "./src/config/dbConnection.js";
 import cookieParser from "cookie-parser";
 import taskRoute from "./src/routes/route.task.js";
 import vigilantRoute from "./src/routes/route.vigilant.js";
-import cors from "cors";
 
 const app = express();
 
@@ -13,7 +12,7 @@ connectiondb();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: 'https://deploy-vercel-frontendt-is.vercel.app', credentials: true }));
+
 app.get("/", (req, res) => res.status(200).send("Bienvenido a nuestro servidor."));
 app.use("/api", mainRouter);
 app.use("/api", taskRoute);
