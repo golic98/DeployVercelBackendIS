17b274c41cc459c7ae78e457f6423ea897a5f5eb
golic98 <00214917@uca.edu.sv>
2025-10-31 13:37:14 -0600

    actualizando user.controller

--- service/src/controllers/user.controllers.js
+++ service/src/controllers/user.controllers.js
@@ -69,8 +69,11 @@ export const login = async (req, res) => {
 
 
 export const logout = (req, res) => {
-  res.cookie('token', "", {
-    expires: new Date(0)
+  res.clearCookie("token", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'none',
+    path: '/',
   });
   return res.sendStatus(200);
 };
