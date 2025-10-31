2db9d7b7461d75c41d363b681eda0b94cd9fa70b
golic98 <00214917@uca.edu.sv>
2025-10-31 12:11:10 -0600

    actualizando user.controller(funcion logout)

--- service/src/controllers/user.controllers.js
+++ service/src/controllers/user.controllers.js
@@ -69,7 +69,7 @@ export const login = async (req, res) => {
 
 
 export const logout = (req, res) => {
-  res.cookie('token', {
+  res.clearCookie('token', {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
     sameSite: 'none',
