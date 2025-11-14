24fb8ec1bda4e26529dbdc9b0f03b49e3b0492e6
golic98 <00214917@uca.edu.sv>
2025-11-13 19:39:42 -0600

    update

--- service/src/controllers/user.controllers.js
+++ service/src/controllers/user.controllers.js
@@ -45,7 +45,8 @@ export const login = async (req, res) => {
     res.cookie('token', token, { 
         httpOnly: true, 
         secure: process.env.NODE_ENV === 'production', 
-        sameSite: 'strict'
+        sameSite: 'none',
+        maxAge: 1000 * 60 * 60 * 24
     });
 
     res.json({
